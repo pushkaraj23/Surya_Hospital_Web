5151bc60590358845d515f16de3b8d7ca3a933bc
pushkaraj23 <pushkaraj232002@gmail.com>
2025-11-21 11:39:21 +0530

    added scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,16 @@ import DepartmentsPage from "./pages/DepartmentsPage";
 import DoctorsPage from "./pages/DoctorsPage";
 import GalleryPage from "./pages/GalleryPage";
 import BlogPage from "./pages/BlogPage";
+import ScrollToTop from "./utils/ScrollToTop";
 import AppointmentBooking from "./components/Appoinment Booking/AppointmentBooking";
-import FeedbackForm from "./components/Feedback Form/FeedbackForm";
 import ContactUs from "./components/ContactUs/ContactUs";
+import FeedbackForm from "./components/Feedback Form/FeedbackForm";
 
 function App() {
   return (
     <div className="bg-mute">
       <Header />
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
